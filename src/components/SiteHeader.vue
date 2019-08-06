5daff3c555199d8037ce08373bde7957eb04574a
Mike McBride <mmcbride1007@icloud.com>
2019-08-06 15:54:25 -0500

    adjust font sizes and weights

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="py-6 sm:py-12">
-    <g-link to="/" class="block text-black font-bold text-2xl lg:text-3xl mb-2">Mike McBride</g-link>
+    <g-link to="/" class="block text-black font-semibold text-3xl mb-2">Mike McBride</g-link>
     <nav class="block site-nav text-xs">
       <NavLink to="/" :active="$route.path === '/'" title="About" />
       <NavLink to="/projects" :active="$route.path.startsWith('/projects')" title="Projects" />
