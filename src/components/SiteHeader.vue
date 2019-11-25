1dbc96f68bcd13e36a1bda6942252a68e7c0d481
Mike McBride <mmcbride1007@icloud.com>
2019-11-25 15:13:26 -0600

    get rid of mobile nav, just be responsive

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,33 +1,16 @@
 <template>
-  <header class="py-6 sm:py-12">
-    <div class="flex justify-between items-center mb-3 md:mb-8">
+  <header class="py-6 sm:py-12 mb-6">
+    <div class="flex justify-between items-center mb-3 mb-8">
       <g-link to="/" class="block font-semibold text-default text-2xl">Mike McBride</g-link>
-      <ToggleDarkMode class="h-8 w-8 hidden md:block" />
-      <button class="block md:hidden w-8 h-8 p-0 bg-transparent text-default" @click="toggleNav">
-        <Bars v-if="!navOpen" />
-        <Close v-else />
-      </button>
+      <ToggleDarkMode class="h-8 w-8 block" />
     </div>
-    <!-- mobile nav -->
-    <nav class="mobile-nav z-10 inset-x-0 bottom-0 fixed flex flex-col md:hidden pl-4 text-lg" v-if="navOpen">
+
+    <nav class="block text-base">
       <NavLink to="/" :active="$route.path === '/'" title="About" />
       <NavLink to="/projects" :active="$route.path.startsWith('/projects')" title="Projects" />
       <NavLink to="/blog" :active="$route.path.startsWith('/blog')" title="Blog" />
       <NavLink to="/uses" :active="$route.path.startsWith('/uses')" title="Uses" />
-      <button
-        class="inline-block mb-4 p-0 text-left text-default font-semibold text-lg bg-transparent"
-        @click="toggleDarkMode">
-        Toggle dark mode
-      </button>
-    </nav>
-    
-    <!-- desktop nav -->
-    <nav class="hidden md:block text-base">
-      <NavLink to="/" :active="$route.path === '/'" title="About" />
-      <NavLink to="/projects" :active="$route.path.startsWith('/projects')" title="Projects" />
-      <NavLink to="/blog" :active="$route.path.startsWith('/blog')" title="Blog" />
-      <NavLink to="/uses" :active="$route.path.startsWith('/uses')" title="Uses" />
-      <div class="border-faded border-b sm:-ml-16 lg:-ml-24 w-screen -mt-px"></div>
+      <div class="border-faded border-b -ml-4 sm:-ml-16 lg:-ml-24 w-screen -mt-px"></div>
     </nav>
   </header>
 </template>
@@ -35,16 +18,12 @@
 <script>
 import NavLink from '@/components/NavLink'
 import ToggleDarkMode from '@/components/ToggleDarkMode'
-import Bars from '@/components/Bars'
-import Close from '@/components/Close'
 
 export default {
   name: 'SiteHeader',
   components: {
     NavLink,
-    ToggleDarkMode,
-    Bars,
-    Close
+    ToggleDarkMode
   },
   computed: {
     navOpen() {
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,12 @@
 <template>
   <Layout>
-    <section class="post-content">
-      <div class="text-center md:text-left mb-8 mt-4">
+    <section class="post-content md:flex">
+      <div class="text-center mb-8 mt-4 md:mr-8">
         <span class="inline-block overflow-hidden border border-faded h-32 w-32 rounded-full dynamic-shadow">
           <Avatar class="w-full h-full rounded-full" />
         </span>
       </div>
-      <p class="mb-8">
+      <p class="mb-8 md:mt-4">
         Hey there! I'm Mike. I'm a web developer based out of St. Louis, MO, 
         where I live with <a href="https://putting-on-love.com" target="_blank" rel="nofollow noreferrer noopener">my amazing wife</a> and our
         <span class="line-through mr-1">daughter</span>
