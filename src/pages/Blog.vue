455488858467da072e2cfa42afc3c33eb7d28d0b
Mike McBride <mmcbride1007@icloud.com>
2020-01-09 14:45:07 -0600

    improve a11y on blog search input

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -2,13 +2,14 @@
   <Layout>
     <h1 class="text-4xl font-medium mb-3">Blog</h1>
     <p class="mb-8">Sometimes I write things. You can read them here.</p>
-    <div class="relative mb-6">
+    <label class="block relative mb-6 search-input">
       <input
+        name="Search blog posts"
         v-model="searchText"
         placeholder="Search blog posts"
-        class="text-base lg:text-sm bg-transparent py-2 pl-8 text-default focus:outline-none search-input w-full" />
+        class="text-base lg:text-sm bg-transparent py-2 pl-8 text-default focus:outline-none w-full" />
       <Search class="inline-block w-4 h-4 absolute search-icon text-default opacity-50" />
-    </div>
+    </label>
     <div class="border-faded border-b left-0 -ml-4 sm:-ml-16 md:-ml-24 lg:-ml-30 w-screen"></div>
     <section>
       <BlogListItem
@@ -55,7 +56,8 @@ export default {
   transform: translateY(-50%);
 }
 
-.search-input:focus + .search-icon {
+.search-input > input:focus + .search-icon,
+.search-input:hover > .search-icon {
   opacity: 1;
 }
 </style>
