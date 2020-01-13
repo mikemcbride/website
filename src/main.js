45f1017e57a97cc150e5a1e65ecbe444cca7ec42
Mike McBride <mmcbride1007@icloud.com>
2020-01-13 13:55:42 -0600

    move inter font to static directory

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import DefaultLayout from '~/layouts/Default.vue'
 import Vuex from 'vuex'
 import '~/assets/tailwind.css'
-import '~/assets/inter/inter.css'
+// import '~/assets/inter/inter.css'
 import ExternalLink from '~/components/ExternalLink.vue'
 
 export default function (Vue, { head, appOptions, router }) {
@@ -48,6 +48,11 @@ export default function (Vue, { head, appOptions, router }) {
   head.meta.push({
     name: 'description',
     content: 'Personal website of Mike McBride'
+  })
+
+  head.link.push({
+    rel: "stylesheet",
+    href: "/inter/inter.css"
   })
 
   Vue.component('Layout', DefaultLayout)
