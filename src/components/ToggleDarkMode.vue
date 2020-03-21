31e1eaa614d0bdbf97e2e9ef1e10e5d2393af20e
Mike McBride <mmcbride1007@gmail.com>
2020-03-20 23:30:30 -0500

    update svgs

--- src/components/ToggleDarkMode.vue
+++ src/components/ToggleDarkMode.vue
@@ -3,8 +3,8 @@
     @click="toggleDarkMode"
     class="border rounded p-3 w-12 h-12 bg-transparent hover:bg-grey-lighter dark-hover:bg-grey-darkest border-transparent focus:outline-none"
     :title="toggleTitle">
-    <svg v-if="isUsingDarkMode" viewBox="0 0 24 24" aria-hidden="true"><g stroke-linejoin="full" stroke-linecap="full" stroke-width="2" fill="none" stroke="currentColor"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.22 4.22l1.42 1.42M18.36 18.36l1.42 1.42M1 12h2M21 12h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42"/></g></svg>
-    <svg v-else viewBox="0 0 24 24" aria-hidden="true"><path fill="currentColor" d="M21.4 13.7c-.8.2-1.6.3-2.4.3-5 0-9-4-9-9 0-.8.1-1.6.3-2.4.1-.3 0-.7-.3-1-.3-.3-.6-.4-1-.3C4.3 2.7 1 7.1 1 12c0 6.1 4.9 11 11 11 4.9 0 9.3-3.3 10.6-8.1.1-.3 0-.7-.3-1-.2-.2-.6-.3-.9-.2z"/></svg>
+    <svg v-if="isUsingDarkMode" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"><path d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
+    <svg v-else fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"><path d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"/></svg>
   </button>
 </template>
 
