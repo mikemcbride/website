4980d0a4f4e3b6b47e889700d1f4aaf9d268f55f
Mike McBride <mike.mcbride@hey.com>
2020-08-10 13:07:49 -0500

    persist dark mode preference

--- www/src/site/_includes/js/checkDarkMode.js
+++ www/src/site/_includes/js/checkDarkMode.js
@@ -1,6 +1,15 @@
 const darkModeButton = document.getElementById('dark-mode-toggle')
+const localStorageKey = 'mikemcbride.dark-mode'
 
 function checkDarkMode() {
+  // first, check if localStorage has a preference.
+  // if so, use that
+  const lsItem = window.localStorage.getItem(localStorageKey)
+  if (lsItem) {
+    return lsItem === 'dark'
+  }
+
+  // if no preference, default to system preference
   return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
 }
 
@@ -22,10 +31,14 @@ function addDarkModeSelector() {
 }
 
 function toggleDarkMode() {
+  // only manually set dark/light mode if they click the button
+  // otherwise, use system preference
   if (document.documentElement.classList.contains('mode-dark')) {
     setLightMode()
+    window.localStorage.setItem(localStorageKey, 'light')
   } else {
     setDarkMode()
+    window.localStorage.setItem(localStorageKey, 'dark')
   }
 }
 
