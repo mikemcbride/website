b75a057ed86f43ad8a83101b20cf8c0801b5bb20
Mike McBride <mike.mcbride@hey.com>
2020-11-01 13:24:41 -0600

    make title link more mobile friendly, update 404 page

--- www/src/components/TitleLink.vue
+++ www/src/components/TitleLink.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="relative z-10 group inline-block mb-4 -ml-4">
-        <a v-if="isExternal" :href="to" class="font-sans font-bold inline-block bg-white rounded px-4 py-2 text-xl md:text-2xl shadow md:shadow-none hover:shadow" v-bind="$attrs">
+    <div class="relative z-10 group inline-block mb-4 md:-ml-4">
+        <a v-if="isExternal" :href="to" class="font-sans font-bold inline-block bg-white rounded px-2 md:px-4 py-2 text-xl md:text-2xl shadow md:shadow-none hover:shadow" v-bind="$attrs">
             <slot></slot>
         </a>
-        <g-link v-else :to="to" class="font-sans font-bold inline-block bg-white rounded px-4 py-2 text-xl md:text-2xl shadow md:shadow-none hover:shadow" v-bind="$attrs">
+        <g-link v-else :to="to" class="font-sans font-bold inline-block bg-white rounded px-2 md:px-4 py-3 md:py-2 text-xl md:text-2xl shadow md:shadow-none hover:shadow" v-bind="$attrs">
             <slot></slot>
         </g-link>
         <span class="-z-1 absolute inset-0 rounded transition-transform ease-linear duration-75 transform -rotate-3 md:rotate-0 group-hover:-rotate-3 bg-gradient-to-r from-aqua to-blue"></span>
