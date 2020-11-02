3156643b65a768a4d151c5f992fd18773e45d7b2
Mike McBride <mike.mcbride@hey.com>
2020-11-02 07:54:53 -0600

    words

--- www/src/components/WhatOthersAreSaying.vue
+++ www/src/components/WhatOthersAreSaying.vue
@@ -5,24 +5,24 @@
         <div class="relative inline-block transform rotate-1 text-blue text-lg md:text-xl ml-3 mb-16">
             <BigQuote />
             <p class="mb-2">An absolute freak athlete. Mike is on another level.</p>
-            <div class="text-right">- LeBron James, good athlete</div>
+            <div class="text-right">- LeBron James</div>
         </div>
         <div class="flex justify-end mb-16">
             <div class="relative transform -rotate-2 text-hot-pink text-lg md:text-xl md:w-2/3 md:mr-12">
                 <BigQuote />
                 <p class="mb-2">Mike writes the most elegant JavaScript I have ever seen. If you’re looking for the pinnacle of web development talent, look no further.</p>
-                <div class="text-right">- Brendan Eich, creator of JavaScript</div>
+                <div class="text-right">- Mark Zuckerberg</div>
             </div>
         </div>
-        <div class="relative text-gray-700 text-lg md:text-xl md:ml-16 md:w-2/3 mb-16">
+        <div class="relative text-gray-700 text-lg md:text-xl md:ml-16 md:w-1/2 mb-16">
             <BigQuote />
             <p class="mb-2">Unbelievably handsome. I mean, have you seen this guy? Just... wow.</p>
-            <div class="text-right">- Ryan Reynolds, handsome actor</div>
+            <div class="text-right">- Ryan Reynolds</div>
         </div>
         <div class="relative transform -rotate-3 text-purple-600 text-lg md:text-xl ml-12 md:mx-auto w-2/3 md:w-2/5 mb-16">
             <BigQuote />
             <p class="mb-2">Nice guy. Moderately funny.</p>
-            <div class="text-right md:mr-8">- Becky McBride, my wife</div>
+            <div class="text-right md:mr-8">- My wife</div>
         </div>
     </div>
 </template>
