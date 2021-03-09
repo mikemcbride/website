0bd119380d0f3ee0e9cb546ba11730189aba196e
Mike McBride <mike.mcbride@hey.com>
2021-03-09 10:41:59 -0600

    fix bad font url

--- components/Head.js
+++ components/Head.js
@@ -16,7 +16,7 @@ const Head = (props) => (
     <link rel="preload" href="/monolisa/MonoLisa-Regular.woff2" as="font" crossOrigin="anonymous" />
     <link rel="preload" href="/monolisa/MonoLisa-Medium.woff2" as="font" crossOrigin="anonymous" />
     <link rel="preload" href="/monolisa/MonoLisa-Bold.woff2" as="font" crossOrigin="anonymous" />
-    <link rel="preload" href="/monolisa/MonoLisa-Italic.woff2" as="font" crossOrigin="anonymous" />
+    <link rel="preload" href="/monolisa/MonoLisa-RegularItalic.woff2" as="font" crossOrigin="anonymous" />
     <link rel="stylesheet" href="/monolisa/monolisa.css" />
     <link rel="stylesheet" href="/inter/inter.css" />
     <meta property="og:url" content={props.url || defaultOGURL} />
