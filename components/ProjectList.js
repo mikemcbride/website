9eba9f2f133185a556c165f22f7629a000781a3d
Mike McBride <mike.mcbride@hey.com>
2021-05-14 10:47:35 -0500

    project list tweaks

--- components/ProjectList.js
+++ components/ProjectList.js
@@ -6,9 +6,15 @@ const ProjectList = ({ projects }) => {
     const extras = projects.filter(p => p.type === 'extra')
     return (
         <>
+            <h3 class="font-bold">Jump to a section:</h3>
+            <ul class="-ml-4 mt-4 mb-16 inline-flex items-center divide-x divide-gray-400">
+                <li class="px-4"><a class="text-blue hover:underline underline md:no-underline" href="#websites">Websites</a></li>
+                <li class="px-4"><a class="text-blue hover:underline underline md:no-underline" href="#utilities">Utilities</a></li>
+                <li class="px-4"><a class="text-blue hover:underline underline md:no-underline" href="#miscellaneous">Miscellaneous</a></li>
+            </ul>
             <section className="mb-16">
-                <h2 className="font-sans font-black text-4xl mb-2 bg-gradient-to-r from-aqua to-blue text-transparent bg-clip-text inline-block">Websites</h2>
-                <p className="prose lg:prose-lg mb-6">
+                <h2 id="websites" className="font-sans font-black text-4xl mb-2 bg-gradient-to-r from-aqua to-blue text-transparent bg-clip-text inline-block">Websites</h2>
+                <p className="prose lg:prose-lg mb-8">
                     I like building websites. Here are a few of the ones I've made.
                 </p>
                 {websites.map((project, index) => (
@@ -17,8 +23,8 @@ const ProjectList = ({ projects }) => {
             </section>
 
             <section className="mb-16">
-                <h2 className="font-sans font-black text-4xl mb-2 bg-gradient-to-r from-blue to-purple text-transparent bg-clip-text inline-block">Utilities</h2>
-                <p className="prose lg:prose-lg mb-6">
+                <h2 id="utilities" className="font-sans font-black text-4xl mb-2 bg-gradient-to-r from-blue to-purple text-transparent bg-clip-text inline-block">Utilities</h2>
+                <p className="prose lg:prose-lg mb-8">
                     I also build a lot of stuff that makes developers jobs easier. Mostly because it makes my own job easier. Check out some of my favorites below and find more on my GitHub or npm.
                 </p>
                 {utilities.map((project, index) => (
@@ -27,8 +33,8 @@ const ProjectList = ({ projects }) => {
             </section>
 
             <section className="mb-16">
-                <h2 className="font-sans font-black text-4xl mb-2 bg-gradient-to-r from-purple to-hot-pink text-transparent bg-clip-text inline-block">Miscellaneous</h2>
-                <p className="prose lg:prose-lg mb-6">
+                <h2 id="miscellaneous" className="font-sans font-black text-4xl mb-2 bg-gradient-to-r from-purple to-hot-pink text-transparent bg-clip-text inline-block">Miscellaneous</h2>
+                <p className="prose lg:prose-lg mb-8">
                     This section is called "miscellaneous" but really it's just some themes I've built. Maybe someday it will have some other stuff too. Who knows. For now, these are the themes I use every day in my editor and terminal. I've made some other themes too, which you can find on GitHub.
                 </p>
                 {extras.map((project, index) => (
--- components/ProjectListItem.js
+++ components/ProjectListItem.js
@@ -7,13 +7,13 @@ const ProjectListItem = ({ project }) => (
         <div className="flex items-center">
             {project.url &&
                 <a href={project.url}
-                    className="text-blue inline-block text-base hover:underline"
+                    className="text-blue inline-block text-base underline md:no-underline hover:underline"
                     target="_blank"
                     rel="nofollow noreferrer noopener">Website</a>
             }
             {project.source &&
                 <a href={project.source}
-                    className={`text-blue inline-block text-base hover:underline ${project.url ? 'pl-2 ml-2 border-l border-gray-500' : ''}`}
+                    className={`text-blue inline-block text-base underline md:no-underline hover:underline ${project.url ? 'pl-4 ml-4 border-l border-gray-400' : ''}`}
                     target="_blank"
                     rel="nofollow noreferrer noopener">Source</a>
             }
