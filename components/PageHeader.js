f4e042869aecc15920af39823252e434f336bba1
Mike McBride <mike.mcbride@hey.com>
2021-06-04 12:04:10 -0500

    wip

--- components/PageHeader.js
+++ components/PageHeader.js
@@ -2,7 +2,7 @@ export default function PageHeader({ children }) {
     return (
         <div className="relative w-full mb-8">
             <h1 className="m-0 mb-1 text-3xl md:text-5xl leading-tight text-black font-sans font-black">{children}</h1>
-            <div className="h-1 bg-gradient-to-r from-hot-pink to-purple"></div>
+            <div className="h-px md:h-1 bg-gradient-to-r from-hot-pink to-purple"></div>
         </div>
     )
 }
--- components/SiteNav.js
+++ components/SiteNav.js
@@ -5,11 +5,11 @@ import NavMenu from './NavMenu'
 const SiteNav = () => (
     <HorizontalPad>
         <nav className="pt-6 sm:pt-12 mb-12 md:mb-16 flex flex-col items-center justify-between md:flex-row text-base">
-            <div className="inline-block relative z-10 group transform -rotate-1">
-                <Link href="/" className="inline-block font-sans font-black text-white bg-blue py-2 px-4 transform -rotate-1 md:rotate-0 group-hover:-rotate-2 transition-transform duration-100 ease-linear uppercase text-4xl rounded-md shadow-md">
+            <div className="md:-ml-4 inline-block relative z-10 group">
+                <Link href="/" className="inline-block font-sans font-black text-black bg-white py-2 px-4 transform shadow-md md:shadow-none -rotate-2 md:rotate-0 group-hover:-rotate-2 group-hover:shadow-md transition-transform duration-100 ease-linear uppercase text-4xl rounded-md">
                     Mike McBride
                 </Link>
-                <div className="absolute inset-0 bg-gradient-to-r from-hot-pink to-purple transform rotate-3 md:rotate-0 group-hover:rotate-2 transition-transform duration-100 ease-linear rounded-md -z-1"></div>
+                <div className="absolute inset-px bg-gradient-to-r from-hot-pink to-purple transform rotate-3 md:rotate-0 group-hover:rotate-2 transition-transform duration-100 ease-linear rounded-md -z-1"></div>
             </div>
             <NavMenu />
         </nav>
