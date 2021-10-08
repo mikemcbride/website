4ada4ce75fabba29f4024e5b833e57bedbad6938
Mike McBride <6248612+mikemcbride@users.noreply.github.com>
2021-10-08 01:53:40 -0500

    Update gumbo.js

--- pages/gumbo.js
+++ pages/gumbo.js
@@ -14,11 +14,11 @@ const Gumbo = ({ title, description }) => (
                 <li>4 boneless skinless chicken breasts or 6-8 thighs</li>
                 <li>2 links Andouille sausage, cut into bite sized pieces</li>
                 <li>3/4 cup flour</li>
-                <li>1/4 cup vegetable oil</li>
+                <li>3/4 cup vegetable oil</li>
                 <li>2 large or 3 small white onions, finely chopped</li>
                 <li>4-5 stalks of celery, finely chopped</li>
                 <li>1 green bell pepper, finely chopped</li>
-                <li>5-6 green onions, finely chopped</li>
+                <li>5-6 green onions, chopped</li>
                 <li>1/2 bunch of fresh parsley, chopped</li>
                 <li>2-3 cloves of minced garlic</li>
                 <li>2 Tbsp Tony Cachere's seasoning</li>
@@ -34,10 +34,10 @@ const Gumbo = ({ title, description }) => (
                 <li>Chop the vegetables and herbs and set aside. I usually do this the night before to make it easy on myself and throw it all in a ziploc bag.</li>
                 <li>Brown the pre-cooked sausage in a cast iron pan. Remove and set aside, do not discard drippings.</li>
                 <li>In the same pan, with the sausage drippings still in, brown the chicken. We're not trying to cook it here, just brown it.</li>
-                <li>In a cast iron pot, make the roux. Cook the flour and oil together over medium heat until it's between the color of peanut butter and a Hershey bar (you can go darker if you wish). Stir constantly so you do not burn it!</li>
+                <li>In a cast iron pot, make the roux. Cook the flour and oil together over medium heat until it's the color of a Hershey bar (you can go darker if you wish). Stir constantly so you do not burn it!</li>
                 <li>When the roux is the desired color, add the Cajun Trinity vegetables. Stir to mix and let the roux wilt the raw vegetables.</li>
                 <li>Add in the chicken broth, stirring well to incorporate.</li>
-                <li>Add the chicken, sausage, and remaining vegetables and seasonings (garlic, green onion, parsley, bay leaves, Tony's, red and black pepper). Stir to blend.</li>
+                <li>Add the chicken, sausage, and remaining vegetables and seasonings except the green onion (garlic, parsley, bay leaves, Tony's, red and black pepper). Stir to blend.</li>
                 <li>Cook for about 2 hours over medium heat with the lid off (or half off) so the broth can thicken. Stir every 20-30 minutes to keep it from burning.</li>
                 <li>When the liquid is at the desired level, you can put the lid on the pot and lower the heat to low and simmer.</li>
                 <li>If the liquid gets too low, you can just add more water or chicken broth. Adjust seasoning to taste.</li>
