9c2f48e5699dd3134149183a77b8fd8c355a1885
Mike McBride <mike.mcbride@hey.com>
2022-08-30 16:05:23 -0500

    update fonts and such

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
                     'sans-serif',
                 ],
                 'serif': [
+                    'Source Serif Pro',
                     'Georgia',
                     'serif',
                 ],
