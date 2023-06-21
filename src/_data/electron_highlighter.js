12693b1e4bd7838f5bfd76ccd0d66e8635420384
Mike McBride <mike.mcbride@hey.com>
2023-06-21 14:24:27 -0500

    update electron-highlighter page

---
+++ src/_data/electron_highlighter.js
@@ -0,0 +1,67 @@
+module.exports = [
+    {
+        "url": "https://github.com/electron-highlighter/nvim",
+        "title": "Neovim",
+        "description": ""
+    },
+    {
+        "url": "https://github.com/electron-highlighter/vscode",
+        "title": "VS Code",
+        "description": ""
+
+    },
+    {
+        "url": "https://github.com/electron-highlighter/fish",
+        "title": "Fish Shell",
+        "description": ""
+
+    },
+    {
+        "url": "https://github.com/electron-highlighter/kitty",
+        "title": "Kitty",
+        "description": ""
+
+    },
+    {
+        "url": "https://github.com/electron-highlighter/hyper",
+        "title": "Hyper",
+        "description": ""
+
+    },
+    {
+        "url": "https://github.com/electron-highlighter/iterm",
+        "title": "iTerm 2",
+        "description": ""
+
+    },
+    {
+        "url": "https://github.com/electron-highlighter/alacritty",
+        "title": "Alacritty",
+        "description": ""
+
+    },
+    {
+        "url": "https://github.com/mikemcbride/insomnia-plugin-extra-themes",
+        "title": "Insomnia",
+        "description": ""
+
+    },
+    {
+        "url": "https://github.com/electron-highlighter/sublime",
+        "title": "Sublime Text",
+        "description": ""
+
+    },
+    {
+        "url": "https://github.com/electron-highlighter/slack",
+        "title": "Slack",
+        "description": ""
+
+    },
+    {
+        "url": "https://github.com/electron-highlighter/terminal",
+        "title": "Terminals",
+        "description": ""
+
+    }
+]
