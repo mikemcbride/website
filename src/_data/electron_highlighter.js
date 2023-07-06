e3676ae81c40572d92520e033592a221f223cd15
Mike McBride <mike.mcbride@hey.com>
2023-07-06 10:13:17 -0500

    add tmux to electron-highlighter

--- src/_data/electron_highlighter.js
+++ src/_data/electron_highlighter.js
@@ -2,66 +2,61 @@ module.exports = [
     {
         "url": "https://github.com/electron-highlighter/nvim",
         "title": "Neovim",
-        "description": ""
+        "description": "A clean, dark Neovim theme written in Lua, with support for lsp, treesitter and lots of plugins"
     },
     {
         "url": "https://github.com/electron-highlighter/vscode",
         "title": "VS Code",
-        "description": ""
-
+        "description": "Electron Highlighter theme for VS Code"
     },
     {
         "url": "https://github.com/electron-highlighter/fish",
         "title": "Fish Shell",
-        "description": ""
-
+        "description": "Electron Highlighter colors in the fish shell"
     },
     {
         "url": "https://github.com/electron-highlighter/kitty",
         "title": "Kitty",
-        "description": ""
-
+        "description": "Electron Highlighter colors for the Kitty terminal emulator"
+    },
+    {
+        "url": "https://github.com/electron-highlighter/tmux",
+        "title": "tmux",
+        "description": "An opinionated tmux config using Electron Highlighter colors"
     },
     {
         "url": "https://github.com/electron-highlighter/hyper",
         "title": "Hyper",
-        "description": ""
-
+        "description": "Electron Highlighter for the Hyper terminal emulator"
     },
     {
         "url": "https://github.com/electron-highlighter/iterm",
         "title": "iTerm 2",
-        "description": ""
-
+        "description": "Electron Highlighter for iTerm 2"
     },
     {
         "url": "https://github.com/electron-highlighter/alacritty",
         "title": "Alacritty",
-        "description": ""
-
+        "description": "Electron Highlighter for the Alacritty terminal emulator"
     },
     {
         "url": "https://github.com/mikemcbride/insomnia-plugin-extra-themes",
         "title": "Insomnia",
-        "description": ""
-
+        "description": "A plugin for Insomnia that adds a bunch of extra themes, including Electron Highlighter"
     },
     {
         "url": "https://github.com/electron-highlighter/sublime",
         "title": "Sublime Text",
-        "description": ""
-
+        "description": "Electron Highlighter syntax theme for Sublime Text 3"
     },
     {
         "url": "https://github.com/electron-highlighter/slack",
         "title": "Slack",
-        "description": ""
-
+        "description": "Sidebar theme for Slack"
     },
     {
         "url": "https://github.com/electron-highlighter/terminal",
         "title": "Terminals",
-        "description": ""
-
+        "description": "Apple Terminal, Gnome Terminal, and Linux Console"
     }
 ]
